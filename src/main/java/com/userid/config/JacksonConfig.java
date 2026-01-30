779f4aeec97a5f2e6021089fa523faf7903d702f
Kosha <igornem1@gmail.com>
2026-01-31 00:03:56 +0200

    ,

--- src/main/java/com/userid/config/JacksonConfig.java
+++ src/main/java/com/userid/config/JacksonConfig.java
@@ -3,12 +3,11 @@ package com.userid.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @Configuration
 public class JacksonConfig {
   @Bean
-  public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
-    return builder.build();
+  public ObjectMapper objectMapper() {
+    return new ObjectMapper().findAndRegisterModules();
   }
 }
